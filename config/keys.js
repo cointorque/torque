6011035f21865101b0f4f195911bed6ec81a32a4
Streple Duck <streple@streple>
2024-08-24 13:17:56 -0400

    all

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword33 = 'mongodb://localhost:27017/mern2';
+dbPassword = 'mongodb+srv://cointorque:TruePass12@cluster0.2vwlk.mongodb.net/CoinTorqueUsers'
+module.exports = {
+    mongoURI: dbPassword
+};
